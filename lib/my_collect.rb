d37c19f66fc2e467cb9ab0a18b3b7eb78174510f
Martinique <me@martidolce.com>
2020-11-14 00:27:41 -0500

    commit tree f1ae67dc4192e0df5b804bdb0c2d99069f795d19

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
 
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i += 1
+  end
+  collect
+end
